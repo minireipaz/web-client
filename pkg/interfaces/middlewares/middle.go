240722b15fd8951df3acc9f8d2fe2feb23bf8c27
jose luis <2064537+sosan@users.noreply.github.com>
2024-08-04 01:30:53 +0200

    cors allowed uris gets from .env
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- pkg/interfaces/middlewares/middle.go
+++ pkg/interfaces/middlewares/middle.go
@@ -1,6 +1,9 @@
 package middlewares
 
 import (
+	"minireipaz/pkg/config"
+	"strings"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
@@ -8,9 +11,11 @@ import (
 
 func Register(app *gin.Engine) {
 	app.Use(otelgin.Middleware("server-frontend"))
+	allowedOriginsEnv := config.GetEnv("CORS_ALLOWED_ORIGINS", "http://localhost:3020,http://localhost:3010")
+	allowedOrigins := strings.Split(allowedOriginsEnv, ",")
 	app.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
-		// AllowOrigins:     []string{"http://localhost:3020", "http://localhost:3010"},
+		// AllowAllOrigins: true,
+		AllowOrigins:     allowedOrigins,
 		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		AllowCredentials: true,
