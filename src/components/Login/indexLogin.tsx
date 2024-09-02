ded3839c8671d2969deefa882e89a0d99371253d
jose luis <2064537+sosan@users.noreply.github.com>
2024-09-02 22:51:24 +0200

    added user info example
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- src/components/Login/indexLogin.tsx
+++ src/components/Login/indexLogin.tsx
@@ -10,10 +10,13 @@ export default function Login({ authenticated, handleLogin }: Props) {
     <div>
       {authenticated === null && <div>Loading...</div>}
       {authenticated === false && (
-        <div>
-          <button onClick={handleLogin}>
+        <div className="flex flex-col gap-4">
+          <button className="bg-slate-400" onClick={handleLogin}>
             Login
           </button>
+          <h2>ayilezt485@nic.edu.pl</h2>
+          <h2>123</h2>
+
         </div>
       )}
       {authenticated && <Navigate to="/callback" />}
