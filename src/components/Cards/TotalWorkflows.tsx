4d08c821674cde1fded4eb59162a2dfb1e3caed3
jose luis <2064537+sosan@users.noreply.github.com>
2024-09-08 23:45:49 +0200

    wrap whitespace
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- src/components/Cards/TotalWorkflows.tsx
+++ src/components/Cards/TotalWorkflows.tsx
@@ -21,7 +21,7 @@ export function TotalWorkflows(props: ContainerProps) {
     <>
       <div className="rounded-lg border bg-card text-card-foreground shadow-sm" >
         <div className="flex flex-col space-y-1.5 p-6" >
-          <h3 className="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight" >Total Workflows</h3>
+          <h3 className="whitespace-pre-wrap text-2xl font-semibold leading-none tracking-tight" >Total Workflows</h3>
           <p className="text-sm" >All workflows created</p>
         </div>
         <div className="p-6 flex items-center justify-between" >
