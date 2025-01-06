c7c498387d3fd353b7e94350ab6e4b7ee2be4687
jose luis <2064537+sosan@users.noreply.github.com>
2025-01-06 01:14:29 +0100

    in cause popup blocked and cannot open window, disabled blocked button test
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- src/components/Credentials/ModalCredential.tsx
+++ src/components/Credentials/ModalCredential.tsx
@@ -132,6 +132,7 @@ export function ModalCredential(props: ContainerProps) {
     );
     if (!authWindow) {
       console.error('Cannot open window');
+      setDisabledButtonTest(false);
       return;
     }
 
